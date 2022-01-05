35c1c7e0a066bc4b5d076818f6cbf62b4dde6c2f
satoteharshal97 <37376689+satoteharshal97@users.noreply.github.com>
2022-01-05 15:34:29 +0530

    Added - Ability to edit  contact in addresbook

--- src/com/example/addressbook/AddressBook.java
+++ src/com/example/addressbook/AddressBook.java
@@ -14,7 +14,7 @@ public class AddressBook {
         int i = 0;
         while (i == 0) {
             System.out.println("\n*------------Menu------------*");
-            System.out.println("1.Add Contact\n" + "2.Exit Loop\n");
+            System.out.println("1.Add Contact\n" + "2.Edit Contact\n" + "3.Exit Loop\n");
             System.out.println("*----------------------------*");
             System.out.print("Choose option:");
             int option = Integer.parseInt(scanner.nextLine());
@@ -22,8 +22,10 @@ public class AddressBook {
                 case 1:
                     addressBook.addContact();
                     break;
-
                 case 2:
+                    addressBook.editContact();
+                    break;
+                case 3:
                     i = 1;
                     break;
                 default:
@@ -53,7 +55,54 @@ public class AddressBook {
         person.setZipcode(Long.parseLong(scanner.nextLine()));
         phoneBook.add(person);
     }
+    public void editContact() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the contact firstName to edit: ");
+        String name = scanner.nextLine();
+        for (Person person : phoneBook) {
 
+            if (name.equalsIgnoreCase(person.getFirstName())) {
+                System.out.println("Which field you want to edit:");
+                System.out.println("1.First Name\n" + "2.Last Name\n" + "3.Email address\n" +
+                        "4.Phone Number\n" + "5.City\n" + "6.Zip Code\n" + "7.State\n" + "8.Exit Loop\n");
+                int field = Integer.parseInt(scanner.nextLine());
+                switch (field) {
+                    case 1:
+                        System.out.println("Enter first name:");
+                        person.setFirstName(scanner.nextLine());
+                        break;
+                    case 2:
+                        System.out.println("Enter last name:");
+                        person.setLastName(scanner.nextLine());
+                        break;
+                    case 3:
+                        System.out.println("Enter email address:");
+                        person.setEmailId(scanner.nextLine());
+                        break;
+                    case 4:
+                        System.out.println("Enter phone number:");
+                        person.setPhoneNumber(Long.parseLong(scanner.nextLine()));
+                        break;
+                    case 5:
+                        System.out.println("Enter city:");
+                        person.setCity(scanner.nextLine());
+                        break;
+                    case 6:
+                        System.out.println("Enter zip code:");
+                        person.setZipcode(Long.parseLong(scanner.nextLine()));
+                        break;
+                    case 7:
+                        System.out.println("Enter state:");
+                        person.setState(scanner.nextLine());
+                        break;
+                    case 8:
+                        System.exit(0);
+                        break;
+
+                }
+            }
+        }
+    }
 }
 
 
